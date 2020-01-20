ff771b781b89cb66f985ca5515f2699321fbe705
nxan <nektarios.xanthakis@eurodyn.com>
2020-01-20 14:45:21 +0200

    Change Project Structure

--- backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/ecomm/dao/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecomm.dao;
-
-import com.ecomm.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
